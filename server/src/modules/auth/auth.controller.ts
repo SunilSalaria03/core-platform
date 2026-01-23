5024b1b1fca7a3a6a9e8de43d983a2650205a44b
Simranjeet kaur <sunil.salaria@lnpinfotech.com>
2026-01-23 18:23:26 +0530

    Auth module backend - login and sign up

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -9,6 +9,8 @@ import { success } from '../../common/utils/response.utils';
 import { Public } from '../../common/decorators/public.decorator';
 import { CookiesService } from '../../infrastructure/cookies/cookies.service';
 import { RefreshTokenDto } from './dto/refresh-token.dto';
+import { AUTH_CONSTANTS } from './constants/auth.constants';
+import { HTTP_STATUS } from 'src/common/constants/http-status.constants';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -27,8 +29,8 @@ export class AuthController {
 
     return success(
       { user: result.user, accessToken: result.accessToken },
-      'Signup successful',
-      201,
+      AUTH_CONSTANTS.SIGNUP_SUCCESS,
+      HTTP_STATUS.CREATED,
     );
   }
 
@@ -41,8 +43,8 @@ export class AuthController {
 
     return success(
       { user: result.user, accessToken: result.accessToken },
-      'Login successful',
-      200,
+      AUTH_CONSTANTS.LOGIN_SUCCESS,
+      HTTP_STATUS.OK,
     );
   }
 
@@ -59,7 +61,7 @@ export class AuthController {
 
     if (!refreshToken) {
       this.cookies.clearRefreshToken(res as unknown as Response);
-      throw new BadRequestException('Refresh token is required');
+      throw new BadRequestException(AUTH_CONSTANTS.REFRESH_TOKEN_REQUIRED);
     }
 
     try {
@@ -67,7 +69,7 @@ export class AuthController {
 
       this.cookies.clearRefreshToken(res as unknown as Response);
 
-      return success({ accessToken: result.accessToken }, 'Token refreshed', 200);
+      return success({ accessToken: result.accessToken }, AUTH_CONSTANTS.REFRESH_TOKEN_SUCCESS, HTTP_STATUS.OK);
     } catch (e) {
       this.cookies.clearRefreshToken(res as unknown as Response);
       throw e;
---
+++ server/src/modules/auth/constants/auth.constants.ts
@@ -0,0 +1,6 @@
+export const AUTH_CONSTANTS = {
+  LOGIN_SUCCESS: 'Login successful',
+  SIGNUP_SUCCESS: 'Signup successful',
+  REFRESH_TOKEN_REQUIRED: 'Refresh token is required',
+  REFRESH_TOKEN_SUCCESS: 'Refresh token successful',
+};
